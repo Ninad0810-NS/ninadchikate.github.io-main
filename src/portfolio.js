272a053f2af2eb008c24bcb9c601e4ba3c57bc9c
ninad0510 <ninaddotchikate10@gmail.com>
2022-06-08 00:34:29 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,276 @@
+import { Icon } from '@iconify/react';
+export const details = {
+    title: "Ninad Chikate",
+    firstname: "Ninad",
+    location:"Nagpur",
+    description:
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Kotlin / Java and some other cool libraries and frameworks.",
+    og: {
+      title: "Ninad chikate Portfolio",
+      type: "website",
+      url: "#",
+    },
+  };
+
+export const skills={
+  skill1:"Application Development",
+  skill1_desc:"⚡  Develop highly interactive User Interfaces for mobile applications",
+  skill12_desc:"⚡ Developing android mobile applications using apps using Kotlin & Java",
+  skill13_desc:"⚡ Creating application backend in Node ",
+  skill2:"Full Stack Development",
+  skill2_desc:"⚡ Building resposive website front end using React-Redux",
+  skill22_desc:"⚡ Creating application backend along with integration of databases such as PHP SQL & MongoDB",
+  skill23_desc:"⚡ Developing highly scalable and maintainable REST APIs using Microservice Architecture ",
+  softwareSkills1: [
+    {
+      skillName: "Java",
+      fontAwesomeClassname: "logos:java",
+      style: {
+        backgroundColor: "transparent",
+      },
+    },
+    {
+      skillName: "NodeJS",
+      fontAwesomeClassname: "simple-icons:node-dot-js",
+      style: {
+        color: "#339933",
+      },
+    },
+    {
+      skillName: "Kotlin",
+      fontAwesomeClassname: "logos:kotlin",
+      style: {
+        color: "#B75EA4",
+      },
+    },
+  ],
+  softwareSkills2: [
+    {
+      skillName: "ReactJS",
+      fontAwesomeClassname: "simple-icons:react",
+      style: {
+        color: "#61DAFB",
+      },
+    },
+    {
+      skillName: "NodeJS",
+      fontAwesomeClassname: "simple-icons:node-dot-js",
+      style: {
+        color: "#339933",
+      },
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "simple-icons:javascript",
+      style: {
+        backgroundColor: "#000000",
+        color: "#F7DF1E",
+      },
+    },
+    {
+      skillName: "Angular",
+      fontAwesomeClassname: "vscode-icons:file-type-angular",
+      style: {
+        color: "#CC6699",
+      },
+    },
+  ],
+  techstack:[
+    {
+      skillName: "Kotlin",
+      fontAwesomeClassname: "logos:kotlin",
+      style: {
+        color: "#B75EA4",
+      },
+    },
+    {
+      skillName: "Java",
+      fontAwesomeClassname: "logos:java",
+      style: {
+        backgroundColor: "transparent",
+      },
+    },
+    {
+      skillName: "ReactJS",
+      fontAwesomeClassname: "simple-icons:react",
+      style: {
+        color: "#61DAFB",
+      },
+    },
+    {
+      skillName: "HTML5",
+      fontAwesomeClassname: "simple-icons:html5",
+      style: {
+        color: "#E34F26",
+      },
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "simple-icons:javascript",
+      style: {
+        backgroundColor: "#000000",
+        color: "#F7DF1E",
+      },
+    },
+    {
+      skillName: "MySQL",
+      fontAwesomeClassname: "ant-design:console-sql-outlined",
+      style: {
+        color: "#2C8EBB",
+      },
+    },
+    {
+      skillName: "MongoDB",
+      fontAwesomeClassname: "simple-icons:mongodb",
+      style: {
+        color: "#52A74B",
+      },
+    },
+    {
+      skillName: "CSS3",
+      fontAwesomeClassname: "fa-css3",
+      style: {
+        color: "#1572B6",
+      },
+    },
+    {
+      skillName: "BootStrap",
+      fontAwesomeClassname: "logos:bootstrap",
+      style: {
+        color: "#CC6699",
+      },
+    },
+    {
+      skillName: "Angular",
+      fontAwesomeClassname: "vscode-icons:file-type-angular",
+      style: {
+        color: "#CC6699",
+      },
+    },
+    {
+      skillName: "NodeJS",
+      fontAwesomeClassname: "simple-icons:node-dot-js",
+      style: {
+        color: "#339933",
+      },
+    },
+    {
+      skillName: "NPM",
+      fontAwesomeClassname: "simple-icons:npm",
+      style: {
+        color: "#CB3837",
+      },
+    },
+    {
+      skillName: "Yarn",
+      fontAwesomeClassname: "simple-icons:yarn",
+      style: {
+        color: "#2C8EBB",
+      },
+    },
+  ]
+}
+
+export const socialMediaLinks = [
+    {
+      name: "Github",
+      link: "https://github.com/ninad0510",
+      fontAwesomeIcon: "fa-github", 
+      backgroundColor: "#181717", 
+    },
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/ninad-chikate-b520aa1bb/",
+      fontAwesomeIcon: "fa-linkedin-in", 
+      backgroundColor: "#0077B5", 
+    },
+    {
+      name: "Facebook",
+      link: "#/",
+      fontAwesomeIcon: "fa-facebook-f", 
+      backgroundColor: "#1877F2", 
+    },
+    {
+      name: "Instagram",
+      link: "#/",
+      fontAwesomeIcon: "fa-instagram", 
+      backgroundColor: "#E4405F", 
+    },
+    // {
+    //   name: "YouTube",
+    //   link: "https://www.youtube.com/channel/UCI0onCVjH8N0CeXTyoxiWfQ",
+    //   fontAwesomeIcon: "fa-youtube", 
+    //   backgroundColor: "#FF0000", 
+    // },
+    {
+      name: "Gmail",
+      link: "mailto:chikate.ninad00@gmail.com",
+      fontAwesomeIcon: "fa-google", 
+      backgroundColor: "#D14836", 
+    },
+    {
+      name: "Twitter",
+      link: "#/",
+      fontAwesomeIcon: "fa-twitter", 
+      backgroundColor: "#1DA1F2", 
+    }
+   
+  ];
+
+export const sociallinks = {
+  git_link:"https://github.com/ninad0510",
+  youtube_link:"https://www.youtube.com/channel/UC-oEC1VxiY2CRPyQ9I0OGxA",
+  facebook_link:"https://www.facebook.com/",
+  ilinkedin_link:"https://www.linkedin.com/in/ninad-chikate-b520aa1bb/",
+  instagram_link:"https://www.instagram.com/",
+  twitter_link:"https://twitter.com/explore",
+  email:"mailto:ninad.chikate00@gmail.com"
+};
+
+export const certifications = {
+      title: "Google Analytics",
+      subtitle: "- Expert in WEB & Marketing Analytics",
+      logo_path: "udemy.png",
+      // certificate_link:
+      //   "https://www.udemy.com/certificate/UC-9034e7c4-dc5d-49e5-9866-75e2ccac3ca5/",
+      alt_name: "Google Analytics | Expert in WEB & Marketing Analytics",
+      color_code: "#F7F7F7",
+
+      // Road To Code 
+      title1: "Front-End Web Development",
+      subtitle1: "- Complete Front End Web Development",
+      logo_path1: "udemy.png",
+      // certificate_link1:
+      //   "https://drive.google.com/file/d/1haGGbzrX1xuOinanFS0WCFQJvGchE5so/view",
+      alt_name1: "Front_End Web Development",
+      color_code1: "#F7F7F7",
+
+       // JETKING 
+       title2: "GTM- Google Tag Manager",
+       subtitle2: "- GTM With Google Analytics includes event tracking",
+       logo_path2: "udemy.png",
+      //  certificate_link2:
+      //    "https://drive.google.com/file/d/1ZBYx-LqIMT23mfpJkAKFl4oyBLznvpA1/view?usp=sharing",
+      //  alt_name2: "Masters in Network Administration + Cloud V2 ",
+       color_code2: "#F7F7F7",
+
+        // Road To Code 
+      title3: "Master Node.js & Deno.js",
+      subtitle3: "- Master Node JS & Deno.js build REST APIs With Node.js",
+      logo_path3: "udemy.png",
+      // certificate_link3:
+      //   "https://drive.google.com/file/d/1y6840e36dFfHETf7SAES0HXXVTUcQy37/view",
+      alt_name3: "Node.js with Rest API's ",
+      color_code3: "#F7F7F7",
+
+};
+
+export const experience_work = {
+  title: "Programer Analyst Trainee",
+  subtitle: " I Currently worked IT company as a Digital Analytics",
+  company: "Cognizant Technology Solutions Ind Pvt. Ltd.",
+  logo_path: "cognizant_logo.png",
+  website_link: "https://www.cognizant.com/in/en",
+  alt_name: "Cognizant logo",
+  color_code: "#F7F7F7",
+};
